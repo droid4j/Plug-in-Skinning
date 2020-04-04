3b0b0b8515ffb2face0f9736fbe0c598306bef1c
zhangpanzhao <zhangpanzhao@okay.cn>
2020-04-04 16:32:40 +0800

    [test] 测试换肤功能

--- app/src/main/java/com/dapan/plug_in_skinning/MainActivity.java
+++ app/src/main/java/com/dapan/plug_in_skinning/MainActivity.java
@@ -2,12 +2,26 @@ package com.dapan.plug_in_skinning;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.dapan.skin.SkinManager;
+import com.dapan.skin.core.SkinLayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SkinLayoutInflater.getInstance().setInflater(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SkinManager.getInstance().init(this);
+    }
+
+    public void changeSkin(View view) {
+        SkinManager.getInstance().loadSkin(SkinManager.skinPath);
+    }
+
+    public void resetSkin(View view) {
+        SkinManager.getInstance().restoreSkin();
     }
 }
